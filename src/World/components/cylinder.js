71434e0102347a3d0b5bd43df52f7901c2707fdf
martinbc24 <116806411+mar7-n@users.noreply.github.com>
2023-07-07 14:46:05 +0200

    part of the wall with symbols

--- src/World/components/cylinder.js
+++ src/World/components/cylinder.js
@@ -4,9 +4,9 @@ import {
   MeshStandardMaterial,
 } from 'https://cdn.skypack.dev/three@0.132.2';
 
-function createCylinder(radiusTop, radiusBottom, height, radialSegments) {
+function createCylinder(radiusTop, radiusBottom, height, radialSegments, texture) {
   const geometry = new CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
-  const material = new MeshStandardMaterial({ color: 'brown' });
+  const material = new MeshStandardMaterial({ map: texture });
 
   const cylinder = new Mesh(geometry, material);
 
--- src/World/components/rectangle.js
+++ src/World/components/rectangle.js
@@ -4,28 +4,15 @@ import {
   Mesh,
   MeshStandardMaterial,
 } from 'https://cdn.skypack.dev/three@0.132.2';
-import * as BufferGeometryUtils from "../../../vendor/BufferGeometryUtils.js";
 
-function createRectangle(length, width, depth, position) {
+function createRectangle(length, width, depth, position, texture) {
   var rectangle;
-  /*if (additional_object != 0) {
-    var geometry = new BoxBufferGeometry(length, width, depth);
-    var additional_object_geometry = new BoxBufferGeometry(additional_object,additional_object,depth);
-    var mergedGeometry = BufferGeometryUtils.mergeGeometries([geometry, additional_object_geometry]);
-    var material = new MeshStandardMaterial({ color: 'brown' });
-    var additional_object_material = new MeshStandardMaterial({ color: 0x000000 });
-    var additional_object_mesh = new Mesh(additional_object_geometry,additional_object_material);
-    rectangle = new Mesh(mergedGeometry,material);
-    rectangle.add(additional_object_mesh);
-  }
-  else {*/
+  
   var geometry = new BoxBufferGeometry(length, width, depth);
-  var material = new MeshStandardMaterial({ color: 'brown' });
+  var material = new MeshStandardMaterial({ map: texture });
   rectangle = new Mesh(geometry, material);
-  //}
 
   rectangle.position.set(position[0],position[1],position[2]);
-  //rectangle.rotation.set(MathUtils.degToRad(angle[0]),MathUtils.degToRad(angle[1]),MathUtils.degToRad(angle[2]));
 
   return rectangle;
 }
